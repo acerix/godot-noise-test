0a122b0061c7bdd9c435de4bacfe3968d5642e85
Dylan Ferris <psient@proton.me>
2025-12-14 21:30:25 -0500

    static

--- main.gd
+++ main.gd
@@ -1,6 +1,12 @@
 extends Control
 
 func _input(_event):
-	# quit when Esc is pressed
+	# Quit when Esc is pressed
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	# Generate a random seed for the shader
+	#$Canvas.material.set_shader_parameter("randi", randi())
+	$Canvas.material.set_shader_parameter("randf", randf())
